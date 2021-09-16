12429575d709fee234fd123500f470ff1b2b643e
KowloonFox <90866923+KowloonFox@users.noreply.github.com>
2021-09-16 13:20:38 -0600

    Add files via upload

---
+++ Data_Module.py
@@ -0,0 +1,11 @@
+#YEEEEEEEEEE
+
+"""KCT Database for Github.test"""
+
+a = 5
+b = 10
+c = 15
+d = 25
+e = 30
+
+
---
+++ Mod database.py
@@ -0,0 +1,13 @@
+
+import Data_Module
+
+num_1 = Data_Module.a
+num_2 = Data_Module.b
+num_3 = Data_Module.c
+num_4 = Data_Module.d
+num_5 = Data_Module.e
+
+print ("we will now do a thing")
+print (num_1, "+", num_2, "=", num_1 + num_2)
+print (num_3, "*", num_4, num_5, "=", num_3 * num_4 * num_5)
+print ("wow! amazing, a thing!")
